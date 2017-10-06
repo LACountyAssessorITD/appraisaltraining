9f892e3fa234e1a9e9a97a20603311c76dfed512
Yining Huang <yininghu@usc.edu>
2017-10-05 17:55:42 -0700

    file upload test files

---
+++ Repo/test/Upload/index.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript" src="lib/jquery/jquery-3.1.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select databse file to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload file" name="submit">
+</form>
+
+</body>
+</html>
+
+
