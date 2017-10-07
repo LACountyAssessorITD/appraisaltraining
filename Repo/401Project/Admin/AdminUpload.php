0d6870687899cdc6dff3e6e399aa765f6e767eea
rusc <assessortestpdf@gmail.com>
2017-10-07 21:08:10 +0000

    [yh] Enable file upload function
    Also Change AdminUpload.html to AdminUpload.php

--- Repo/401Project/Admin/AdminUpload.php
+++ Repo/401Project/Admin/AdminUpload.php
@@ -56,16 +56,63 @@
 
 			<p>Update New</p>
 			<div class="accordionCont">
-				<h2>一行字</h2>					
+				<h2>Select databse file to upload:</h2>
+				<form action="" method="post" enctype="multipart/form-data">
+				    <input type="file" name="fileToUpload" id="fileToUpload">
+				    <input type="submit" value="Upload file" name="submit">
+				</form></br>
+				<?php
+				if(isset($_POST["submit"])) {
+					if ($_FILES["fileToUpload"]["name"] == "") {
+						echo "Please select a file";
+						return;
+					}
+				    $target_dir = "D:/temp/";
+				    $FileType = pathinfo($_FILES["fileToUpload"]["name"],PATHINFO_EXTENSION);
+				    $target_file = $target_dir . basename("BOE".'_'.time().$FileType);
+				    $uploadOk = 1;
+
+
+				    // Check if file already exists
+				    if (file_exists($target_file)) {
+				        echo "Sorry, file already exists.";
+				        $uploadOk = 0;
+				    }
+				    // Check file size
+				    if ($_FILES["fileToUpload"]["size"] > 50000000) { // if the file is larger than 50 MB
+				        echo "Sorry, your file is too large.";
+				        $uploadOk = 0;
+				    }
+				    // Allow certain file formats
+				    if($FileType != "mdb" && $FileType != "xlsx" && $FileType != "csv") {
+				        echo "Sorry, only mdb, xlsx and csv files are allowed.";
+				        $uploadOk = 0;
+				    }
+				    // Check if $uploadOk is set to 0 by an error
+				    if ($uploadOk == 0) {
+				        echo "Sorry, your file was not uploaded.";
+				    // if everything is ok, try to upload file
+				    } else {
+				        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+				        	echo '<script>alert("File Uploaded!")</script>';
+				            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+				            echo " Renamed as ".basename("BOE".'_'.time().$FileType);
+				            //echo '<script>alert("File Uploaded!")</script>';
+				        } else {
+				            echo "Sorry, there was an error uploading your file.". $_FILES["fileToUpload"]['error'];
+				        }
+				    }
+				}
+				?>
 			</div>
 
 			<p>Restore Previous</p>
 			<div class="accordionCont">
-				<h2>兩行字</h2>	
-				<h3>兩行字</h3>					
+				<h2>兩行字</h2>
+				<h3>兩行字</h3>
 			</div>
 
-		</div>	
+		</div>
 
 	</div>
 
