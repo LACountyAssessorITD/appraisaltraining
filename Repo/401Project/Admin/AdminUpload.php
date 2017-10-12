8a9c88715cdd165614b9d18b419d5e59be50327b
Yueh-Hsun Lin <yuehhsul@usc.edu>
2017-10-12 12:02:05 -0700

    [nelson] common stylesheet and css removal for all stylesheets

--- Repo/401Project/Admin/AdminUpload.php
+++ Repo/401Project/Admin/AdminUpload.php
@@ -4,6 +4,7 @@
 <head>
 	<title>LAC Website</title>
 	<link rel="stylesheet" href="AdminUploadStyle.css">
+	<link rel="stylesheet" href="../CSS/DefaultUI.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
