3a8062573506e99c10f798c6d00d26287bd8b7af
Yueh-Hsun Lin <yuehhsul@usc.edu>
2017-10-13 23:53:37 -0700

    [nelson] more ui changes

--- Repo/401Project/Admin/AdminUpload.php
+++ Repo/401Project/Admin/AdminUpload.php
@@ -5,6 +5,7 @@
 	<title>LAC Website</title>
 	<link rel="stylesheet" href="AdminUploadStyle.css">
 	<link rel="stylesheet" href="../CSS/DefaultUI.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
