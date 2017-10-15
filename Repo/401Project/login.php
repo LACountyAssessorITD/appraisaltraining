b897fc9de63e0ee0d8b82fc941c2521bb2215217
Yining <yininghu@usc.edu>
2017-10-14 17:39:25 -0700

    [yh] adding sign in page

---
+++ Repo/401Project/login.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_POST["submit"])) {
+	/*
+	$success = false;
+	if(empty($_POST['username']))
+	{
+		$this->HandleError("UserName is empty!");
+		$success = false;
+	} else if(empty($_POST['password']))
+	{
+		$this->HandleError("Password is empty!");
+		$success = false;
+	}
+	$username = trim($_POST['username']);
+	$password = trim($_POST['password']);
+	if(!$this->CheckLoginInDB($username,$password))
+	{
+		return false;
+	}
+	session_start();
+	$_SESSION[$this->GetLoginSessionVar()] = $username;
+	return true;
+	*/
+}
+?>
