4ccc7412ac733f07b299b6e67af84032c365f2cc
rusc <assessortestpdf@gmail.com>
2017-10-16 07:37:48 +0000

    [yh] final debug before testing on site

--- Repo/401Project/LDAP/login.php
+++ Repo/401Project/LDAP/login.php
@@ -10,36 +10,37 @@ session_start();
 
 	if (!authenticateUser($ldapusername, $ldappassword)) {
 		// $_SESSION["logged_in"] = FALSE;
-		// header("Location: " . LOGIN_ERROR_URL);		
+		// header("Location: " . LOGIN_ERROR_URL);
 		echo "authenticateUser failure";
-	} else {	
-		
+	} else {
+
 		// Get display name from LDAP
 		// Get Employee ID from LDAP
 		$server = LDAP_SERVER_NAME;
 		$ldap = ldap_connect($server);
 		$userid=$_POST["username"];
-		if ($ldap) {	
+		if ($ldap) {
 			ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
 			ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
-			$bind = ldap_bind($ldap, $ldapusername, $ldappassword);	
+			$bind = ldap_bind($ldap, $ldapusername, $ldappassword);
 			$basedn = "OU=ASSR,DC=laassessor,DC=co,DC=la,DC=ca,DC=us";
 			$filter = '(samaccountname='.$userid.')';
 			$attributes = array("displayname");
 			$result = ldap_search($ldap, $basedn, $filter, $attributes);
 			if (FALSE !== $result) {
 				$info = ldap_get_entries($ldap, $result);
-				if($info["count"] > 0) {				
+				if($info["count"] > 0) {
 					for ($i=0; $i<$info["count"]; $i++) {
 						 $_SESSION['NAME']=$info[$i]["displayname"][0];
+						 echo "The Name is ".$_SESSION['NAME'];
 					}
 				}
 				else {
 					echo "error when try to connect LDAP 38";
-				}			
+				}
 			}
 		}
-		
+
 		$empid;
 		// See if it is an appraiser
 		$serverName = SQL_SERVER_NAME;
@@ -78,5 +79,5 @@ session_start();
 		sqlsrv_free_stmt($stmt);
 		sqlsrv_close($conn);
 
-
+	}
 ?>
