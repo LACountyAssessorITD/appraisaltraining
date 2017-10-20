2ae43f04a53cb6b0ae57b1719ed2c1a725780fc3
Yueh-Hsun Lin <yuehhsul@usc.edu>
2017-10-19 17:20:58 -0700

    [nelson]fix9

--- Repo/401Project/Admin/AdminHomeJS.js
+++ Repo/401Project/Admin/AdminHomeJS.js
@@ -78,7 +78,7 @@ $(document).ready(function(){
 
             $.when(getResultArray()).done(function() {
                 globalint += 1;
-                $("#homeTab").text($(this).prop("tagName"));
+                $("#homeTab").text($(this).prop("class"));
                 var DPBContHtml_top = "<div class='DPBCont'>\
                                         <div class='tableWrap'>\
                                             <form class='leftInput'>\
