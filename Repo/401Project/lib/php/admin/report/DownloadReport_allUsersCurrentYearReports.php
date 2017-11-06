52d8352bc949e7838ff98db69cb5a925d023c17e
Yining Huang <yininghu@usc.edu>
2017-11-06 09:28:20 -0800

    [yh] test download

--- Repo/401Project/lib/php/admin/report/DownloadReport_allUsersCurrentYearReports.php
+++ Repo/401Project/lib/php/admin/report/DownloadReport_allUsersCurrentYearReports.php
@@ -4,10 +4,10 @@ This Code dynamically generate individual PDF (Specific Year Report)
 @ Yining Huang
 */
 
-include_once "../../../constants.php";
-include_once "../../../session.php";
+include_once "../../constants.php";
+include_once "../../session.php";
 //session_start();
-include_once "pdfTemplate_allUsersCurrentYearReports.php";
+include_once "../../report_template/pdfTemplate_specificYear.php";
 ///////////////////////////////////////////////////////////////////
 /* Access Database here */
 $serverName = SQL_SERVER_NAME;
@@ -26,19 +26,22 @@ if( $conn === false )
      echo "Unable to connect.</br>";
      die( print_r( sqlsrv_errors(), true));
 }
-
 $totalcarryover = 0;
-$all_id=array(7292,10226,3664);
-$year = 2017;
+
+$all_certids = $_SESSION['view_certNo']; // maybe an array-
+$certid = -1;
+$year = $_SESSION['view_specific_year']; // get the current year
 
 ///////////////////////////////////////////////////////////////////
 
 $pdf = new myPDF('L','mm','A4');
-for ($i=0; $i < $all_id.size(); $i ++) {
+for ($i = 0; $i < count($all_certids); $i++) {
+	$certid = $all_certids[$i];
 	$pdf->AliasNbPages();
 	$pdf->AddPage();
-	$pdf->generate($conn,$all_id[$i]);
+	$pdf->generate($conn);
 }
+
 sqlsrv_close($conn);
 $name = "LACounty".$year."_AnnualTraining.pdf";
 $pdf->Output($name,'D');
