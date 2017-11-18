a3d9a0fb434e790e60ef37c8f27efa7171fbaa19
rusc <assessortestpdf@gmail.com>
2017-11-17 17:30:57 -0800

    [nelson] new faq everything

--- Repo/401Project/Admin/AdminFAQ.php
+++ Repo/401Project/Admin/AdminFAQ.php
@@ -4,19 +4,12 @@
 <html>
 <head>
 	<title>LAC Website</title>
-	<!-- <link rel="stylesheet" href="AdminFAQStyle.css"> -->
 	<link rel="stylesheet" href="../CSS/DefaultUI.css">
 	<link rel="stylesheet" href="../CSS/FAQStyle.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-	<!-- <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"> -->
-  	<!-- <link rel="stylesheet" href="/resources/demos/style.css"> -->
-  	<!-- <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script> -->
-
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  	<!-- <script src="https://code.jquery.com/jquery-1.12.4.js"></script> -->
+  	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 	<script src="AdminFAQJS.js"></script>
 </head>
 
@@ -26,38 +19,15 @@
 
 	<div class="content">
 
-		<div class="faqContent" id="accordion">
+		<div class="faqContent">
 
-			<p class="pFiltersLabel">How do I filter what report I want to see?
-			</p>
+			<div class="faqSetDiv">
+				<button class="questionButton"><span class='downArrowSpan'><i class='fa fa-angle-down' aria-hidden='true'></i></span> How do I filter what report I want to see?</button>
 
-			<div class="employeeFilters">
-
-				<div class="dropDownFilter">
-					<p>
-						You may use the selection tools to the left of the report, under “Fiscal Year” to select if you would like to view either one specific fiscal year’s report, or a range of fiscal years.
-						<br><br>
-						For one year, select “Specific Year” and then choose the fiscal year you would like to view, and click “Generate Report”.
-						<br><br>
-						For a range of years, select “Range of Years” and specify the time range you would like to view. “From Year” will begin from Jan 1st of that year, and “To Year” will end on Dec 31st of that year. Click “Generate Report” to view your reports.
-						<br><br>
-					</p>
-				</div>
-			</div>
-
-			<p class="pFiltersLabel">How do I filter what report I want to see?
-			</p>
-
-			<div class="employeeFilters">
-
-				<div class="dropDownFilter">
-					<p>
-						You may use the selection tools to the left of the report, under “Fiscal Year” to select if you would like to view either one specific fiscal year’s report, or a range of fiscal years.
-						<br>
-						For one year, select “Specific Year” and then choose the fiscal year you would like to view, and click “Generate Report”.
-						<br>
-						For a range of years, select “Range of Years” and specify the time range you would like to view. “From Year” will begin from Jan 1st of that year, and “To Year” will end on Dec 31st of that year. Click “Generate Report” to view your reports.
-					</p>
+				<div class="answerDiv">
+					<p>The answer.The answer.The answer.The answer.The answer.The answer.The answer.
+						The answer.The answer.The answer.The answer.The answer.The answer.
+					The answer.The answer.The answer.The answer.The answer.The answer.The answer.</p>
 				</div>
 			</div>
 
@@ -66,14 +36,6 @@
 	</div>
 
 	<?php include '../common/Footer.php'; ?>
-	<!-- <div class="footer">
-		<p class="links">
-			<span id="contactUs"><a href="#">Contact Us</a></span>
-			<span id="Disclaimer"><a href="#">Disclaimer</a></span>
-			<span id="FAQs"><a href="#">FAQs</a><span></span>
-		</p>
-		<p>2017 - Present © Los Angeles County Office of the Assessor</p>
-	</div> -->
 </body>
 
 </html>
--- Repo/401Project/Admin/AdminFAQJS.js
+++ Repo/401Project/Admin/AdminFAQJS.js
@@ -4,44 +4,20 @@ $(document).ready(function(){
       e.preventDefault();
   });
 
-  // $(function(){
-  //       $.datepicker.setDefaults(
-  //         $.extend( $.datepicker.regional[ '' ] )
-  //       );
-  //       $( '#datepicker' ).datepicker();
-  //     });
 
-
-/*--------------------------------------------------------------------------------------------*/
-/*------------------------------Display Accordions--------------------------------------------*/
-/*--------------------------------------------------------------------------------------------*/
-
-  $.fn.togglepanels = function(){
-      return this.each(function(){
-        $(this).addClass("ui-accordion ui-accordion-icons ui-widget ui-helper-reset")
-      .find("p")
-        .addClass("ui-accordion-header ui-helper-reset ui-state-default ui-corner-top ui-corner-bottom")
-        .hover(function() {
-          $(this).toggleClass("ui-state-hover");
-        })
-        .prepend('<span class="ui-icon ui-icon-triangle-1-e"></span>')
-        .click(function() {
-          $(this)
-            .toggleClass("ui-accordion-header-active ui-state-active ui-state-default ui-corner-bottom")
-            .find("> .ui-icon").toggleClass("ui-icon-triangle-1-e ui-icon-triangle-1-s").end()
-            .next().slideToggle();
-          return false;
-        })
-        .next()
-          .addClass("ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom")
-          .hide();
-      });
-    };
-
-    $("#accordion").togglepanels();
-
-/*--------------------------------------------------------------------------------------------*/
-/*------------------------------Display Accordions END----------------------------------------*/
-/*--------------------------------------------------------------------------------------------*/
+  $(".questionButton").on("click",function() {
+      if($(this).data("clicked")) {
+        $(this).closest(".faqSetDiv").find(".answerDiv").hide();
+        $(this).data("clicked",false);
+        $(this).find("i").remove();
+        $(this).find("span").append("<i class='fa fa-angle-down' aria-hidden='true'></i>")
+      }
+      else {
+        $(this).closest(".faqSetDiv").find(".answerDiv").show();
+        $(this).data("clicked",true);
+        $(this).find("i").remove();
+        $(this).find("span").append("<i class='fa fa-angle-up' aria-hidden='true'></i>")
+      }
+  });
 
 });
