f947ff8df3d063acb721ffec70e980afd928e8f9
Yining <yininghu@usc.edu>
2017-11-26 21:15:38 -0800

    [yh] ldap info Item number fix

--- Repo/401Project/lib/php/LDAP/getLdapInfo.php
+++ Repo/401Project/lib/php/LDAP/getLdapInfo.php
@@ -1,7 +1,32 @@
 <?php
+/* 
+	This code retrieve LDAP information for both Admin and User
+	Handle the post method (by specifying an employee number) from an ajax call
+	@ Yining Huang
+
+	0 - display name (full name)
+	1 - Email
+	2 - Magager Name
+	3 - Given Name (firstName)
+	4 - phone number
+	5 - Department / Pay location
+	6 - Title
+	7 - Item Number
+
+*/
 include_once "../constants.php";
 include_once "authenticate.php";
 session_start();
+
+	function getItemNumber($title) {
+		if ($title == "Appraiser") return 1962;
+		else if (strpos( $title, "Principal Appraiser" )) return 1970;
+		else if (strpos( $title, "Appraiser Specialist" )) return 1965;
+		else if (strpos( $title, "Supervising Appraiser" )) return 1968;
+		else if (strpos( $title, "Appraiser Assistant" )) return 1958;
+		else if (strpos( $title, "Appraiser Trainee" )) return 1960;
+		else if (strpos( $title, "Chief Appraiser" )) return 1974;
+	}
 
 	$look_up_username = (string)$_POST['empNo'];
 	$ldapusername = "laassessor"."\\".$_SESSION['USERNAME'];
@@ -25,18 +50,32 @@ session_start();
 			$inforesult  = array();
 			if($info["count"] > 0) {
 				for ($i=0; $i<$info["count"]; $i++) {
+					/*
+						0 - display name (full name)
+						1 - Email
+						2 - Magager Name
+						3 - Given Name (firstName)
+						4 - phone number
+						5 - Department / Pay location
+						6 - Title
+						7 - Item Number
+					*/
 					$inforesult[] = $info[$i]["displayname"][0];
 					$inforesult[] =$info[$i]["mail"][0];
+
 					$managerArrOne = explode(",", (string)$info[$i]["manager"][0], 3);
 					$tempName = (string)$managerArrOne[0]." ".(string)$managerArrOne[1];
 					$managerArrTwo = explode("=", $tempName, 2);
 					$inforesult[] = (string)$managerArrTwo[1];
 					// $inforesult[] = $info[$i]["manager"][0];
+
 					$inforesult[] = $info[$i]["givenname"][0];
 					$inforesult[] = $info[$i]["telephonenumber"][0];	 // phone number
 					$inforesult[] = $info[$i]["department"][0];	// pay location
-					$inforesult[] =$info[$i]["title"][0];// title
 
+					$title_string = (string)$info[$i]["title"][0];
+					$inforesult[] = $title_string; // title
+					$inforesult[] = getItemNumber($title_string);
 				}
 				echo json_encode($inforesult);
 			}
