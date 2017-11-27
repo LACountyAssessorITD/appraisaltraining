7d7619d58b63873d1b38cd5bd6bc302fd9dbb4ad
rusc <assessortestpdf@gmail.com>
2017-11-27 02:01:08 -0800

    [nelson] changes to faq page structure

--- Repo/401Project/Admin/AdminFAQ.php
+++ Repo/401Project/Admin/AdminFAQ.php
@@ -10,7 +10,7 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-	<script src="AdminFAQJS.js"></script>
+	<script src="../common/FAQJS.js"></script>
 </head>
 
 <body>
@@ -22,7 +22,9 @@
 		<div class="faqContent">
 
 			<div class="faqSetDiv">
-				<button class="questionButton"><span class='downArrowSpan'><i class='fa fa-angle-down' aria-hidden='true'></i></span> How do I filter what report I want to see?</button>
+				<button class="questionButton">
+					<span class='downArrowSpan'><i class='fa fa-angle-down' aria-hidden='true'></i></span> How do I filter what report I want to see?
+				</button>
 
 				<div class="answerDiv">
 					<p>The answer.The answer.The answer.The answer.The answer.The answer.The answer.
--- Repo/401Project/User/UserFAQ.php
+++ Repo/401Project/User/UserFAQ.php
@@ -10,49 +10,40 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-	<script src="UserFAQJS.js"></script>
+	<script src="../common/FAQJS.js"></script>
 </head>
 
 <body>
 
-	<?php include "../common/UserTop.php" ?>
+	<?php include "../common/UserTop.php"; ?>
 
 	<div class="content">
 
-		<div class="faqContent" id="accordion">
+		<div class="faqContent">
 
-				<p class="pFiltersLabel">How do I filter what report I want to see?
-				</p>
+			<div class="faqSetDiv">
+				<button class="questionButton">
+					<span class='downArrowSpan'><i class='fa fa-angle-down' aria-hidden='true'></i></span> How do I filter what report I want to see?
+				</button>
 
-				<div class="employeeFilters">
-
-					<div class="dropDownFilter">
-						<p>
-							You may use the selection tools to the left of the report, under “Fiscal Year” to select if you would like to view either one specific fiscal year’s report, or a range of fiscal years.
-							<br><br>
-							For one year, select “Specific Year” and then choose the fiscal year you would like to view, and click “Generate Report”.
-							<br><br>
-							For a range of years, select “Range of Years” and specify the time range you would like to view. “From Year” will begin from Jan 1st of that year, and “To Year” will end on Dec 31st of that year. Click “Generate Report” to view your reports.
-							<br><br>
-						</p>
-					</div>
+				<div class="answerDiv">
+					<p>The answer.The answer.The answer.The answer.The answer.The answer.The answer.
+						The answer.The answer.The answer.The answer.The answer.The answer.
+					The answer.The answer.The answer.The answer.The answer.The answer.The answer.</p>
 				</div>
+			</div>
 
-				<p class="pFiltersLabel">How do I filter what report I want to see?
-				</p>
+			<div class="faqSetDiv">
+				<button class="questionButton">
+					<span class='downArrowSpan'><i class='fa fa-angle-down' aria-hidden='true'></i></span> How do I filter what report I want to see?
+				</button>
 
-				<div class="employeeFilters">
-
-					<div class="dropDownFilter">
-						<p>
-							You may use the selection tools to the left of the report, under “Fiscal Year” to select if you would like to view either one specific fiscal year’s report, or a range of fiscal years.
-							<br>
-							For one year, select “Specific Year” and then choose the fiscal year you would like to view, and click “Generate Report”.
-							<br>
-							For a range of years, select “Range of Years” and specify the time range you would like to view. “From Year” will begin from Jan 1st of that year, and “To Year” will end on Dec 31st of that year. Click “Generate Report” to view your reports.
-						</p>
-					</div>
+				<div class="answerDiv">
+					<p>The answer.The answer.The answer.The answer.The answer.The answer.The answer.
+						The answer.The answer.The answer.The answer.The answer.The answer.
+					The answer.The answer.The answer.The answer.The answer.The answer.The answer.</p>
 				</div>
+			</div>
 
 		</div>
 
